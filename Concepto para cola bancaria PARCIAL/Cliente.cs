d1265edba30f88c4038d96846350a6ae68d4e8d9
alvaradonatan30-lgtm <alvaradonatan30@gmail.com>
2026-08-09 17:42:47 -0600

    Add files via upload

---
+++ Concepto para cola bancaria PARCIAL/Cliente.cs
@@ -0,0 +1,11 @@
+﻿public class Cliente
+{
+    public int NumeroTurno { get; set; }
+    public string Nombre { get; set; }
+
+    public Cliente(int numeroturno, string nombre)
+    {
+        NumeroTurno = numeroturno;
+        Nombre = nombre;
+    }
+}
---
+++ Concepto para cola bancaria PARCIAL/Turnos.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Turno
+{
+    private Queue<Cliente> cola = new();
+    private int turnoproximo = 1;
+
+    public Cliente AgregarCliente(string nombre)
+    {
+        Cliente cliente = new(turnoproximo, nombre);
+
+        cola.Enqueue(cliente);
+
+        turnoproximo ++;
+
+        return cliente;
+    }
+
+    public Cliente AtenderProximo()
+    {
+        if (cola.Count == 0)
+        {
+            return null;
+        }
+
+        return cola.Dequeue();
+    }
+
+    public Cliente ObtenerProximo()
+    {
+        if (cola.Count == 0)
+        {
+            return null;
+        }
+
+        return cola.Peek();
+    }
+
+    public int CantidadEnEspera()
+    {
+        return cola.Count;
+    }
+}
